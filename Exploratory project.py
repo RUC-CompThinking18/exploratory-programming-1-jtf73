3d1fd50f3300ee8136b265ab56e21b30894e79ee
Jaleel <jtf73@scarletmail.rutger.edu>
2018-10-04 16:02:31 -0700

    Added comments

--- Exploratory project.py
+++ Exploratory project.py
@@ -1,7 +1,10 @@
 def double(t):
 #defined x as a list
     x = []
-
+    f = []
+    d = []
+    #Made new list in order to organize other list and return all of them at once
+    Azcr = []
     #t = [30, 18, 24, 76]
     for element in t:
         if element in t != int
@@ -10,5 +13,10 @@ def double(t):
 
         if element in t = int:
             x += (element * 8/2)
-
-    return x
+            f += (element - 12)
+            d += (element + 4)
+#Now my code will return one big list with all the changes made to the original list
+    Azcr += x
+    Azcr += f
+    Azcr += d
+    return Azcr
