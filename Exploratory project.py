94cfb16895287a60e98813f98ec440abeab174d1
Jaleel <jtf73@scarletmail.rutger.edu>
2018-10-04 15:57:59 -0700

    Added file

---
+++ Exploratory project.py
@@ -0,0 +1,14 @@
+def double(t):
+#defined x as a list
+    x = []
+
+    #t = [30, 18, 24, 76]
+    for element in t:
+        if element in t != int
+            print "Numbers only."
+            return
+
+        if element in t = int:
+            x += (element * 8/2)
+
+    return x
